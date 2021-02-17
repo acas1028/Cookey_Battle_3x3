fb355f629e4ffe50365bc10cf073f57aa28d75b5
acas1028 <63082364+acas1028@users.noreply.github.com>
2021-02-17 14:38:43 +0900

    0217 장정훈병합
    프롤로그 트루엔딩 배드엔딩
    기타등등 폰트변경

---
+++ 3x3 prototype/Assets/Script/ScoreEvaluation/ScoreEvaluationSceneChange.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreEvaluationSceneChange : MonoBehaviour
+{
+    public int stageSelectScene;
+    public int badEndingScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoNextScene()
+    {
+        switch(GameManager.instance.GetStageLevel())
+        {
+            case 1:
+                if (GameManager.instance.GetStage1State() == 0)
+                {
+                    SceneManager.LoadScene(badEndingScene);
+                }
+                else
+                    LoadingSceneManager.LoadScene(stageSelectScene);
+                break;
+            case 2:
+                if (GameManager.instance.GetStage2State() == 0)
+                {
+                    SceneManager.LoadScene(badEndingScene);
+                }
+                else
+                    LoadingSceneManager.LoadScene(stageSelectScene);
+                break;
+            case 3:
+                if (GameManager.instance.GetStage3State() == 0)
+                {
+                    SceneManager.LoadScene(badEndingScene);
+                }
+                else
+                    LoadingSceneManager.LoadScene(stageSelectScene);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
